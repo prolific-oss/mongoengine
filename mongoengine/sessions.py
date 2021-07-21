2c1b90be91bcf69763bb41ab03d2be3b8c51ea3b
Jonas <jonas.anso@prolific.co>
2021-07-21 07:05:30 +0200

    TRU-2836: Make session thread-safe
    In production this should not be an issue but when testing using threads it helps. Alternatively we could use multiprocessing that creates a new process and behaves like production

--- mongoengine/sessions.py
+++ mongoengine/sessions.py
@@ -1,29 +1,33 @@
+import threading
 from typing import Optional
 
 from pymongo.client_session import ClientSession
 
 from mongoengine.connection import DEFAULT_CONNECTION_NAME
 
-_sessions = {}
+_sessions = threading.local()
 
 
 def set_local_session(db_alias: str, session: ClientSession):
-    _sessions[key_local_session(db_alias)] = session
+    _sessions.__setattr__(key_local_session(db_alias), session)
 
 
 def get_local_session(
     db_alias: str = DEFAULT_CONNECTION_NAME,
 ) -> Optional[ClientSession]:
-    return _sessions.get(key_local_session(db_alias))
+    try:
+        return _sessions.__getattribute__(key_local_session(db_alias))
+    except AttributeError:
+        return None
 
 
 def clear_local_session(db_alias: str = DEFAULT_CONNECTION_NAME):
-    _sessions.pop(key_local_session(db_alias), None)
+    _sessions.__delattr__(key_local_session(db_alias))
 
 
 def clear_all():
     global _sessions
-    _sessions = {}
+    _sessions = threading.local()
 
 
 def key_local_session(db_alias):
